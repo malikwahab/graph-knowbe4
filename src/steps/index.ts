296a9940dc9875c058d52e3042c85db04e149141
Malik Adeyi <malik.adeyi@gmail.com>
2023-07-11 11:09:58 -0400

    Add dependent steps for campaigns

--- src/steps/index.ts
+++ src/steps/index.ts
@@ -1,10 +1,12 @@
 import { accountSteps } from './account';
+import { groupSteps } from './groups';
 import { trainingCampaignSteps } from './trainingCampaigns';
 import { trainingEnrollmentSteps } from './trainingEnrollments';
 import { userSteps } from './users';
 
 const integrationSteps = [
   ...accountSteps,
+  ...groupSteps,
   ...userSteps,
   ...trainingCampaignSteps,
   ...trainingEnrollmentSteps,
